fada237e466cd1f6ce606f6211d4b687c5f9ff05
Iqbalbbx <iqbal.fauzi@bobobox.com>
2025-11-09 18:13:50 +0700

    feature(portfolio): Test Github Pages

--- composeApp/src/webMain/kotlin/com/uziro/portfolio/Greeting.kt
+++ composeApp/src/webMain/kotlin/com/uziro/portfolio/Greeting.kt
@@ -4,6 +4,7 @@ class Greeting {
     private val platform = getPlatform()
 
     fun greet(): String {
-        return "Hello, ${platform.name}!"
+//        return "Hello, ${platform.name}!"
+        return "Hello, Iqbal Fauzi!"
     }
 }
