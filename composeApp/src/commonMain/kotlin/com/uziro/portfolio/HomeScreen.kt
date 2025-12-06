7dad19693b875013c41d41a5d44da87f17fbdd67
Iqbalbbx <iqbal.fauzi@bobobox.com>
2025-12-06 08:48:21 +0700

    feature(home): Add homescreen

--- composeApp/src/commonMain/kotlin/com/uziro/portfolio/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/uziro/portfolio/HomeScreen.kt
@@ -2,10 +2,13 @@ package com.uziro.portfolio
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.uziro.portfolio.section.HeaderSection
+import com.uziro.portfolio.section.ProfileSection
 
 @Composable
 fun HomeScreen() {
@@ -13,10 +16,13 @@ fun HomeScreen() {
         modifier = Modifier.fillMaxSize(),
         topBar = {
             HeaderSection(modifier = Modifier.fillMaxWidth())
-//            HeaderSection(modifier = Modifier.fillMaxWidth())
         }
     ) {
-
+        LazyColumn(modifier = Modifier.fillMaxSize().padding(it)) {
+            item {
+                ProfileSection()
+            }
+        }
     }
 
 }
